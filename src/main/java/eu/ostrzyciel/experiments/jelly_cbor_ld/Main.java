fadc48bc3af49a74242f65c5477a65ae188884ff
Ostrzyciel <ostrzycielnozyczek@gmail.com>
2025-03-30 16:45:46 +0200

    It works!

--- src/main/java/eu/ostrzyciel/experiments/jelly_cbor_ld/Main.java
+++ src/main/java/eu/ostrzyciel/experiments/jelly_cbor_ld/Main.java
@@ -2,40 +2,63 @@ package eu.ostrzyciel.experiments.jelly_cbor_ld;
 
 import com.apicatalog.cborld.CborLd;
 import com.apicatalog.jsonld.JsonLd;
+import com.apicatalog.jsonld.JsonLdError;
+import com.apicatalog.jsonld.document.Document;
 import com.apicatalog.jsonld.document.JsonDocument;
 import com.apicatalog.jsonld.document.RdfDocument;
+import com.apicatalog.jsonld.loader.DocumentLoader;
+import com.apicatalog.jsonld.loader.DocumentLoaderOptions;
 import com.apicatalog.rdf.RdfDataset;
 import com.apicatalog.rdf.RdfDatasetSupplier;
 import eu.ostrzyciel.jelly.convert.titanium.TitaniumJellyReader;
 import jakarta.json.Json;
 import jakarta.json.JsonObject;
+import org.apache.commons.io.input.CountingInputStream;
 
+import java.net.URI;
 import java.util.zip.GZIPInputStream;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    private static final DocumentLoader CBOR_LD_LOADER = new DocumentLoader() {
+        private static JsonDocument context = null;
 
-        JsonObject context = null;
-        try (var is = Main.class.getResourceAsStream("/context.jsonld")) {
-            // parse with jakarta
-            var parser = Json.createParser(is);
-            parser.next();
-            context = parser.getObject();
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.exit(1);
+        static {
+            try (var is = Main.class.getResourceAsStream("/context.jsonld")) {
+                // parse with jakarta
+                var parser = Json.createParser(is);
+                parser.next();
+                context = JsonDocument.of(parser.getObject());
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
         }
+
+        @Override
+        public Document loadDocument(URI url, DocumentLoaderOptions options) throws JsonLdError {
+            // Hack: just return the context we have loaded...
+            return context;
+        }
+    };
+
+    public static void main(String[] args) throws Exception {
+        var contextUri = Main.class.getResource("/context.jsonld").toURI();
+        JsonObject context = Json.createObjectBuilder()
+            .add("@context", contextUri.toString())
+            .build();
 
         var inputFileName = "/assist-iot-weather-graphs_10k.jelly.gz";
         try (
             var is = Main.class.getResourceAsStream(inputFileName);
-            var gzipIs = new GZIPInputStream(is)
+            var gzipIs = new GZIPInputStream(is);
+            var cis = new CountingInputStream(gzipIs)
         ) {
             TitaniumJellyReader jellyReader = TitaniumJellyReader.factory();
-            while (gzipIs.available() > 0) {
+            int i = 0;
+            long lastByteCount = 0;
+            while (cis.available() > 0) {
                 var rdfDatasetSupplier = new RdfDatasetSupplier();
-                jellyReader.parseFrame(rdfDatasetSupplier, gzipIs);
+                jellyReader.parseFrame(rdfDatasetSupplier, cis);
                 RdfDataset rdfDataset = rdfDatasetSupplier.get();
                 JsonObject jsonLdDocument = JsonLd.fromRdf(RdfDocument.of(rdfDataset))
                     .get()
@@ -44,16 +67,32 @@ public class Main {
                     .compact(JsonDocument.of(jsonLdDocument), JsonDocument.of(context))
                     .compactArrays(true).get();
 
-                // Doesn't work. Context with multiple entries is not supported.
-                byte[] cborLdBytes = CborLd.createEncoder().build().encode(jsonLdCompacted);
+                byte[] cborLdBytes = CborLd.createEncoder()
+                    .loader(CBOR_LD_LOADER)
+                    .build()
+                    .encode(jsonLdCompacted);
+
+
+                if (i % 10 == 0) {
+                    System.out.println("frame\tJelly\tJSON-LD\tJSON-LD (compact)\tCBOR-LD");
+                }
+
+                long byteCount = cis.getByteCount();
+                System.out.println(i + "\t" + (byteCount - lastByteCount) + "\t" +
+                        jsonLdDocument.toString().length() + "\t" +
+                        jsonLdCompacted.toString().length() + "\t" +
+                        cborLdBytes.length
+                );
+                lastByteCount = byteCount;
+
+                if (i > 100) {
+                    break;
+                }
+                i++;
             }
         } catch (Exception e) {
             e.printStackTrace(); // We are just experimenting here...
             System.exit(1);
         }
-    }
-
-    private static void createCborLdEncoder() {
-        //
     }
 }
